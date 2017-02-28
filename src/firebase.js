e2fb2884bc1ddec64c6eadfad94e687d6f5eb4b5
Larisa Moroz <larisa.moroz@gmail.com>
2017-02-28 17:14:53 +0800

    Firebase added

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+import * as firebase from 'firebase';
+
+const config = {
+    apiKey: 'AIzaSyA_e8-8Wo8pp7nAzo9BoHVw4aCDEMCs1Ls',
+    authDomain: 'react-goal-coach.firebaseapp.com',
+    databaseURL: 'https://react-goal-coach.firebaseio.com',
+    storageBucket: 'react-goal-coach.appspot.com',
+    messagingSenderId: '135907737048'
+};
+
+export const firebaseApp = firebase.initializeApp(config);
--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, browserHistory } from 'react-router';
+import { firebaseApp } from './firebase';
 
 import App from './components/App';
 import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
 
+firebaseApp.auth().onAuthStateChanged(user => {
+    if (user) {
+        console.log('user has signed as', user);
+    } else
+        console.log('user has signed out or still needs to sign in');
+});
 
 ReactDOM.render(
     <Router path="/" history={browserHistory}>
