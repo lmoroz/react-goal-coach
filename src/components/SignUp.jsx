1710bd28b65cebf982cae1c1b23a5ee7c409f472
Larisa Moroz <larisa.moroz@gmail.com>
2017-02-28 16:39:06 +0800

    Setting up

---
+++ src/components/SignUp.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+
+class SignUp extends Component {
+    render () {
+        return (
+            <div>App</div>
+        );
+    }
+}
+
+
+export default SignUp;
